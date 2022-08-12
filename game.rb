ac45b7d412052f4f5017ad4d8efcb5466aa82ce1
Nicholas <nicholasjoven@gmail.com>
2022-08-12 11:54:06 -0400

    added gemfile for sqlite 3, created game.db

--- game.rb
+++ game.rb
@@ -2,6 +2,21 @@
 # Players have a deck (a deck is an array of cards)
 # Cards have stats
 
+require 'securerandom'
+require 'sqlite3'
+
+db = SQLite3::Database.new("game.db")
+
+# Create a table
+rows = db.execute <<-SQL
+  create table numbers (
+    name varchar(30),
+    val int
+  );
+SQL
+
+p rows
+
 class Player
     attr_accessor :name, :email, :password, :deck
     def initialize(name, email, password, deck = [])
@@ -12,14 +27,18 @@ class Player
     end
 end
 
-class Card 
-   def initiailize(name, top, left, bottom, right, element = nil, img=nil)
-    @name = name
-    @top = top
-    @left = left
-    @bottom = bottom
-    @right = right
-    @element = element
-    @img = img
-   end
+# In order to save this information, we would need to create a Cards table in a database
+# When we create a card, we would store that card object in the database
+class Card
+    attr_accessor :name, :top, :left, :bottom, :right, :element, :img
+    def initialize(name, top, left, bottom, right, element = nil, img = nil)
+        @name = name
+        @top = top
+        @left = left
+        @bottom = bottom
+        @right = right
+        @element = element
+        @img = img
+        @card_id = SecureRandom.uuid
+    end
 end
